3c3f8d190455ca9a6ca8f7238e140e45752e0589
Sam Skinner <sam.sjskinner@gmail.com>
2021-07-16 09:28:35 +1000

    new layout - needs fix for rounding differently

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,45 +1,16 @@
-win = hs.window.focusedWindow()
-frame = win:frame()
-screen = win:screen()
-max = screen:frame()
+local WindowManager = require('WindowManager')
 
-hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "J", function()
-    if frame.w == max.w / 3 and frame.x == max.x then
-        frame.w = max.w * 2 / 3
-    else
-        frame.w = max.w / 3
+-- Automatic reloading of hammerspoon config on change
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
     end
-
-    frame.x = max.x
-    frame.y = max.y
-    frame.h = max.h
-    win:setFrame(frame) end)
-
-
-hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "K", function()
-    if frame.w == max.w / 3 and frame.x == max.w / 3 then
-        frame.w = max.w * 2 / 3
-        frame.x = max.w * 1 / 6
-    else
-        frame.w = max.w / 3
-        frame.x = max.w * 1 / 3
+    if doReload then
+        hs.reload()
     end
-
-    frame.y = max.y
-    frame.h = max.h
-    win:setFrame(frame)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "L", function()
-    if frame.w == max.w / 3 and frame.x == max.w * 2 / 3 then
-        frame.w = max.w * 2 / 3
-        frame.x = max.w / 3
-    else
-        frame.w = max.w / 3
-        frame.x = max.w * 2 / 3
-    end
-     
-    frame.y = max.y
-    frame.h = max.h
-    win:setFrame(frame)
-end)
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME").."/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
