6ac2d3439b199e851e6102d6d3d6c1ce69a61fb7
Sam Skinner <sam.sjskinner@gmail.com>
2021-07-15 15:01:32 +1000

    Finished hammerspoon setup and basic window functionality

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,30 +1,45 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "J", function()
-    local win = hs.window.focusedWindow()
-    local frame = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
+win = hs.window.focusedWindow()
+frame = win:frame()
+screen = win:screen()
+max = screen:frame()
 
-    frame.x = max.x 
-    frame.y = max.y
-    frame.h = max.h
-    if frame.w == max.w then
-        frame.w = max.w * 2 / 3 -- not going to 2/3 of the width
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "J", function()
+    if frame.w == max.w / 3 and frame.x == max.x then
+        frame.w = max.w * 2 / 3
     else
         frame.w = max.w / 3
     end
-    win:setFrame(frame)
-end)
+
+    frame.x = max.x
+    frame.y = max.y
+    frame.h = max.h
+    win:setFrame(frame) end)
 
 
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "K", function()
-    local win = hs.window.focusedWindow()
-    local frame = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
+    if frame.w == max.w / 3 and frame.x == max.w / 3 then
+        frame.w = max.w * 2 / 3
+        frame.x = max.w * 1 / 6
+    else
+        frame.w = max.w / 3
+        frame.x = max.w * 1 / 3
+    end
 
-    frame.x = max.x --this needs to be 1/3 across the screen 
     frame.y = max.y
     frame.h = max.h
-    frame.w = max.w
+    win:setFrame(frame)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "L", function()
+    if frame.w == max.w / 3 and frame.x == max.w * 2 / 3 then
+        frame.w = max.w * 2 / 3
+        frame.x = max.w / 3
+    else
+        frame.w = max.w / 3
+        frame.x = max.w * 2 / 3
+    end
+     
+    frame.y = max.y
+    frame.h = max.h
     win:setFrame(frame)
 end)
