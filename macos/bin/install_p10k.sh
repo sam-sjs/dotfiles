170810443750468cadf91a9c6fa59a247ae767af
Sam Skinner <sam.sjskinner@gmail.com>
2022-12-08 13:35:58 +1100

    control p10k config

--- macos/bin/install_p10k.sh
+++ macos/bin/install_p10k.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ln -s $HOME/dotfiles/macos/powerlevel10k $HOME
